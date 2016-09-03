cc4b5bd1716668f959add2e188e6437a7f7999df
reckbo <reckbo@bwh.harvard.edu>
2016-09-03 22:01:57 +1000

    ENH: Add parser for scoring table

--- tagsoup/Main.hs
+++ tagsoup/Main.hs
@@ -1,15 +1,18 @@
-import Text.XML.HXT.Core
-import Text.HandsomeSoup
-import Data.Time
-import Data.Maybe
+import           Data.Maybe
+import           Data.Time
+import           Text.HandsomeSoup
+import           Text.XML.HXT.Core
+import           Data.Tree.NTree.TypeDefs
+
+trim :: String -> String
+trim = unwords . words
 
 data MatchInfo =
-  MatchInfo { round :: Int,
-              venue :: String,
-              date :: LocalTime,
+  MatchInfo { round      :: Int,
+              venue      :: String,
+              date       :: LocalTime,
               attendance :: Int
-            }
-  deriving Show
+            } deriving Show
 
 readMatchInfo :: [String] -> MatchInfo
 readMatchInfo [round, venue, date, attendance]
@@ -21,14 +24,25 @@ readDate t = fromJust $ parseTimeM True defaultTimeLocale fmt t'
     t' = takeWhile (/='(') t
     fmt = "%a, %e-%b-%Y %l:%M %p"
 
-trim :: String -> String
-trim = unwords . words
-
-matchInfoArr = (css "table:first-child" >>> css "tr:first-child" >>> css "td:nth-child(2)" >>> (deep getText)) >. (readMatchInfo . fmap trim . getElems [1,3,6,8])
+matchInfoArr :: IOSLA (XIOState ()) (NTree XNode) MatchInfo
+matchInfoArr = (css "table:first-child" >>>
+                css "tr:first-child" >>>
+                css "td:nth-child(2)" >>>
+                (deep getText)) >.
+               (readMatchInfo . fmap trim . getElems [1,3,6,8])
   where getElems is xs = map (xs !!) is
 
+scoringLineArr :: ArrowXml a => a (NTree XNode) String
+scoringLineArr =  css "td" >>>
+                  listA (deep getText >>. unwords)
+
+scoringTableArr = (css "table:nth-child(8)") >>>
+                  css "tr" >>>
+                  listA scoringLineArr
+
 main = do
-  html <- readFile "test.html"
+  html <- readFile "test2.html"
   let doc = readString [withParseHTML yes, withWarnings no] html
-  r <- runX $ doc >>> matchInfoArr
+  -- r <- runX $ doc >>> matchInfoArr
+  r <- runX $ doc >>> scoringTableArr
   print r
