6d63b08fd59a3cb974749266d2476d6e655bf8f0
reckbo <reckbo@bwh.harvard.edu>
2016-09-04 13:59:42 +1000

    ENH: Add ScoringEvent type and read function

--- tagsoup/Main.hs
+++ tagsoup/Main.hs
@@ -1,13 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.List
+import           Data.List.Split
 import           Data.Maybe
-import           Data.Time
+import           Data.Time (LocalTime, parseTimeM, defaultTimeLocale)
+import           Data.Tree.NTree.TypeDefs
 import           Text.HandsomeSoup
 import           Text.XML.HXT.Core
-import           Data.Tree.NTree.TypeDefs
-import Data.List.Split
-import Data.List
-
-trim :: String -> String
-trim = unwords . words
+import Control.Applicative
 
 data MatchInfo =
   MatchInfo { round      :: Int,
@@ -16,6 +15,48 @@ data MatchInfo =
               attendance :: Int
             } deriving Show
 
+type Player = String
+type Team = String
+
+data ScoreType = Goal | Behind | RushedBehind
+  deriving Show
+
+data TeamType = Home | Away
+  deriving Show
+
+data Teams = Teams
+  { home :: Team
+  , away :: Team }
+
+data ScoringEvent = ScoringEvent
+  { teamType :: TeamType
+  , time :: Int
+  , scoreType :: ScoreType
+  , scorer :: Maybe Player
+  }
+  deriving Show
+
+readScoringEvent :: [String] -> ScoringEvent
+readScoringEvent xs = case head xs of
+  "\160" -> uncurry3 (ScoringEvent Away) $ fromLine $ reverse xs
+  _ -> uncurry3 (ScoringEvent Home) $ fromLine xs
+  where
+   fromLine :: [String] -> (Int, ScoreType, Maybe Player)
+   fromLine (description:t':_) =
+      let t = readTime t'
+      in
+        if description == "Rushed Behind"
+          then (t, RushedBehind, Nothing)
+          else case reverse . words $ description of
+                "behind":name -> (t, Behind, Just $ unwords name)
+                "goal":name -> (t, Goal, Just $ unwords name)
+                _ -> error $ "Unexpected goal type in: " ++ description
+   fromLine xs = error $ "couldnt parse " ++ (unwords xs)
+
+
+trim :: String -> String
+trim = unwords . words
+
 readMatchInfo :: [String] -> MatchInfo
 readMatchInfo [round, venue, date, attendance]
   = MatchInfo (read round) venue (readDate date) (read attendance)
@@ -23,35 +64,55 @@ readMatchInfo [round, venue, date, attendance]
 readDate :: String -> LocalTime
 readDate t = fromJust $ parseTimeM True defaultTimeLocale fmt t'
   where
-    t' = takeWhile (/='(') t
+    t' = trim $ takeWhile (/='(') t
+    trim = unwords . words
     fmt = "%a, %e-%b-%Y %l:%M %p"
 
 matchInfoArr :: IOSLA (XIOState ()) (NTree XNode) MatchInfo
 matchInfoArr = (css "table:first-child" >>>
                 css "tr:first-child" >>>
                 css "td:nth-child(2)" >>>
-                (deep getText)) >.
-               (readMatchInfo . fmap trim . getElems [1,3,6,8])
+               removeAllWhiteSpace >>>
+                (deep getText))
+               >. (readMatchInfo . fmap trim . getElems [1,3,5,7])
   where getElems is xs = map (xs !!) is
 
-scoringLineArr :: ArrowXml a => a (NTree XNode) String
+-- scoringLineArr :: ArrowXml a => a (NTree XNode) [ScoringEvent]
 scoringLineArr =  css "td" >>>
                   listA (deep getText >>. unwords)
 
-scoringTableArr = (css "table:nth-child(8)") >>>
-                  css "tr" >>>
-                  listA scoringLineArr
+scoringTableArr =
+  (css "table:nth-child(8)")
+  >>> css "tr"
+  >>> listA scoringLineArr
 
+scoringTableTeamsArr :: IOSLA (XIOState ()) (NTree XNode) (String, String)
+scoringTableTeamsArr =
+  (css "table:nth-child(8)" >>>
+  css "tr:nth-child(2)"
+  >>> removeAllWhiteSpace
+  >>> css "th"
+  //> getText)
+  >. (liftA2 (,) head last)
+
+readTime :: String -> Int
+readTime s =
+  let
+    rd = read :: String -> Int
+    [min,sec] = rd . init <$> words s
+  in
+    60*min + sec
 getScore = do
-  html <- readFile "test2.html"
+  html <- readFile "test.html"
   let doc = readString [withParseHTML yes, withWarnings no] html
   -- r <- runX $ doc >>> matchInfoArr
-  r <- runX $ doc >>> scoringTableArr
+  r <- runX $ doc >>> scoringTableTeamsArr
+  r <- runX $ doc >>>  scoringTableArr
+  -- r <- runX $ doc >>> scoringTableTeamsArr
   let
-    dropLast = reverse . tail . reverse
-    r' = tail . tail . dropLast $ r  -- last and first 2 rows
-    p = and . map ("quarter" `isInfixOf`)
-    goals = wordsBy p r'
-    quarters = filter p r'
-  return $ zip quarters goals
-  -- return $ quarters
+    r' = tail . tail . init $ r  -- last and first 2 rows
+    pred = and . map ("quarter" `isInfixOf`)
+    events = (fmap readScoringEvent) <$> wordsBy pred r'
+    quarters = filter pred r'
+  return $ zip quarters events
+  -- return r
